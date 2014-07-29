f4da0f0a33c2429864be87d6f1f6fd6a885a9534
Junji Tago <junji.tago@space-elephant.com>
2014-07-30 08:53:43 +0900

    add a documentation

--- SESlideTableViewCell/lib/SESlideTableViewCell.h
+++ SESlideTableViewCell/lib/SESlideTableViewCell.h
@@ -10,12 +10,27 @@
 
 @class SESlideTableViewCell;
 
+/**
+ A state of SESlideTableViewCell that tells the buttons of which side are displayed.
+ */
 typedef NS_ENUM(NSInteger, SESlideTableViewCellSlideState) {
+	/**
+	 The cell shows no button.
+	 */
 	SESlideTableViewCellSlideStateCenter,
+	/**
+	 The cell shows the buttons of the left side.
+	 */
 	SESlideTableViewCellSlideStateLeft,
+	/**
+	 The cell shows the buttons of the right side.
+	 */
 	SESlideTableViewCellSlideStateRight,
 };
 
+/**
+ A side of SESlideTableViewCell.
+ */
 typedef NS_ENUM(NSInteger, SESlideTableViewCellSide) {
 	SESlideTableViewCellSideLeft,
 	SESlideTableViewCellSideRight,
@@ -26,13 +41,36 @@ typedef NS_ENUM(NSInteger, SESlideTableViewCellSide) {
  */
 @protocol SESlideTableViewCellDelegate <NSObject>
 @optional
+/**
+ Tells the delegate that a button of the left side is triggered.
+ 
+ @param cell The cell informing the delegate of the event.
+ @param buttonIndex The index of the button which is triggered.
+ */
 - (void)slideTableViewCell:(SESlideTableViewCell*)cell didTriggerLeftButton:(NSInteger)buttonIndex;
+/**
+ Tells the delegate that a button of the right side is triggered.
+ 
+ @param cell The cell informing the delegate of the event.
+ @param buttonIndex The index of the button which is triggered.
+ */
 - (void)slideTableViewCell:(SESlideTableViewCell*)cell didTriggerRightButton:(NSInteger)buttonIndex;
+/**
+ Asks the delegate if the cell can be a slide-state.
+ 
+ @return YES if the cell can be the state, otherwise NO.
+ @param cell The cell that is making this request.
+ @param slideState The state that the cell want to be.
+ */
 - (BOOL)slideTableViewCell:(SESlideTableViewCell*)cell canSlideToState:(SESlideTableViewCellSlideState)slideState;
 @end
 
 /**
  SESlideTableViewCell is a subclass of UITableViewCell which shows utility buttons with sliding it.
+ 
+ SESlideTableViewCell class itself has only core interfaces.
+ It is easy to use SESlideTableViewCell (ButtonUtility) extension.
+ Or you can create your own convenient extensions with this core interfaces.
  */
 @interface SESlideTableViewCell : UITableViewCell
 
@@ -41,10 +79,31 @@ typedef NS_ENUM(NSInteger, SESlideTableViewCellSide) {
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier;
 
+/**
+ Adds a button to the cell.
+ 
+ the button view is stretched to fit the height of the cell and the width of the button.
+ 
+ @param button A view that is shown as a button.
+ @param buttonWidth The width of the button
+ @param backgroundColor The color of the background of the button.
+ @param side A side which the button is added to.
+ */
 - (void)addButton:(UIView*)button buttonWidth:(CGFloat)buttonWidth backgroundColor:(UIColor*)backgroundColor side:(SESlideTableViewCellSide)side;
 
+/**
+ Removes all the buttons of the left or right side.
+ 
+ @param side A side the buttons of which will be removed.
+ */
 - (void)removeAllButtonsWithSide:(SESlideTableViewCellSide)side;
 
+/**
+ Set the cell to the slide-state.
+ 
+ @param slideState A state to which the cell is made to be.
+ @param animated YES if the state change is happened with animation, otherwise NO.
+ */
 - (void)setSlideState:(SESlideTableViewCellSlideState)slideState animated:(BOOL)animated;
 
 @end
@@ -59,13 +118,65 @@ typedef NS_ENUM(NSInteger, SESlideTableViewCellSide) {
 - (void)removeAllLeftButtons;
 - (void)removeAllRightButtons;
 
+/**
+ Adds a text button. The width of the button is automatically set by its text length.
+ 
+ @param text A text of button.
+ @param textColor A color of text.
+ @param backgroundColor A color of background of the button.
+ @param side A side where the button is added.
+ */
 - (void)addButtonWithText:(NSString*)text textColor:(UIColor*)textColor backgroundColor:(UIColor*)backgroundColor side:(SESlideTableViewCellSide)side;
 
+/**
+ Adds an image button.
+ 
+ The width of the button is automatically set by its image width.
+ The height of the image must be fit in the height of the cell.
+ 
+ @param image A image of the button.
+ @param backgroundColor A color of background of the button.
+ @param side A side where the button is added.
+ */
 - (void)addButtonWithImage:(UIImage*)image backgroundColor:(UIColor*)backgroundColor side:(SESlideTableViewCellSide)side;
 
+/**
+ Adds a text button to the left side.
+ 
+ @param text a text of the button.
+ @param textColor a color of text of the button.
+ @param backgroundColor a color of the background of the button
+ */
 - (void)addLeftButtonWithText:(NSString*)text textColor:(UIColor*)textColor backgroundColor:(UIColor*)backgroundColor;
+
+/**
+ Adds a text button to the right side.
+ 
+ @param text a text of the button.
+ @param textColor a color of text of the button.
+ @param backgroundColor a color of the background of the button
+ */
 - (void)addRightButtonWithText:(NSString*)text textColor:(UIColor*)textColor backgroundColor:(UIColor*)backgroundColor;
+
+/**
+ Adds an image button to the left side.
+ 
+ The width of the button is automatically set by its image width.
+ The height of the image must be fit in the height of the cell.
+ 
+ @param image A image of the button.
+ @param backgroundColor A color of background of the button.
+ */
 - (void)addLeftButtonWithImage:(UIImage*)image backgroundColor:(UIColor*)backgroundColor;
+/**
+ Adds an image button to the right side.
+ 
+ The width of the button is automatically set by its image width.
+ The height of the image must be fit in the height of the cell.
+ 
+ @param image A image of the button.
+ @param backgroundColor A color of background of the button.
+ */
 - (void)addRightButtonWithImage:(UIImage*)image backgroundColor:(UIColor*)backgroundColor;
 
 @end
--- readme.md
+++ readme.md
@@ -4,4 +4,50 @@ SESlideTableViewCell
 <p align="center"><img src="http://i.imgur.com/NUJ9Hts.gif"/></p>
 <p align="center"><img src="http://i.imgur.com/ic1fwxp.gif"/></p>
 
-A subclass of UITableViewCell that shows buttons which is shown by swiping the cell.
+A subclass of UITableViewCell that shows buttons with swiping it.
+
+## Features
+* Portable. Only one header and one source file are included.
+* Supports Swift language.
+
+## Requirements
+
+This supports iOS 7 and later.
+It also works iOS 8 and it can be used in Swift language.
+
+## How to Set up
+
+Use CocoaPods to set up SESlideTableViewCell.
+```
+platform :ios, "7.0"
+pod 'SESlideTableViewCell'
+```
+
+See http://cocoapods.org/ for more information about CocoaPods.
+
+## Getting Started
+
+You can setup your SESlideTableViewCell in your UITableViewController like this.
+```objc
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	NSString * const CELL_ID = @"Cell";
+	SESlideTableViewCell* cell = (SESlideTableViewCell*)[tableView dequeueReusableCellWithIdentifier:CELL_ID];
+	if (!cell) {
+		cell = [[SESlideTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CELL_ID];
+		cell.delegate = self;
+		[cell addRightButtonWithText:@"Hello" textColor:[UIColor whiteColor] backgroundColor:[UIColor colorWithHue:0.0/360.0 saturation:0.8 brightness:0.9 alpha:1.0]];
+		[cell addRightButtonWithText:@"World!!" textColor:[UIColor whiteColor] backgroundColor:[UIColor colorWithHue:180.0/360.0 saturation:0.8 brightness:0.9 alpha:1.0]];
+	}
+	return cell;
+}
+```
+
+You can know which button is triggered with a SESlideTableViewCellDelegate method.
+```objc
+- (void)slideTableViewCell:(SESlideTableViewCell*)cell didTriggerRightButton:(NSInteger)buttonIndex {
+	NSLog(@"right button triggered:%d", (int)buttonIndex);
+}
+```
+# License
+
+MIT license.
